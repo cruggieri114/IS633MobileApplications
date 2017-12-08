063518a1be5c5731e2b1974c54a589f8aa298e8b
cruggieri114 <32877728+cruggieri114@users.noreply.github.com>
2017-12-08 11:09:52 -0600

    added fieldType

--- Assignment6/js/Assignment6.js
+++ Assignment6/js/Assignment6.js
@@ -660,9 +660,9 @@ function SearchContact(){
     var options      = new ContactFindOptions();
     options.filter   = lastname;
     options.multiple = true;
-    options.desiredFields = [navigator.contacts.fieldType.id, navigator.contacts.displayName, navigator.contacts.phoneNumber, navigator.contacts.emails];
+    options.desiredFields = [navigator.contacts.fieldType.id, navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.phoneNumber, navigator.contacts.fieldType.emails];
     options.hasPhoneNumber = true;
-    var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+    var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumber, navigator.contacts.fieldType.emails];
     navigator.contacts.find(fields, onSuccess, onError, options);
     
 function onSuccess(contacts) {
