31c8c8d19178202ed86cac0c07e7c90e005b056f
cruggieri114 <32877728+cruggieri114@users.noreply.github.com>
2017-12-08 14:48:00 -0600

    forgot to change desired fields

--- Assignment6/js/Assignment6.js
+++ Assignment6/js/Assignment6.js
@@ -660,7 +660,7 @@ function SearchContact(){
     var options      = new ContactFindOptions();
     options.filter   = lastname;
     options.multiple = true;
-    options.desiredFields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers];
+    options.desiredFields = [navigator.contacts.fieldType.givenName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers];
     options.hasPhoneNumber = true;
     var fields = [navigator.contacts.fieldType.displayName];
     navigator.contacts.find(fields, onSuccess, onError, options);
