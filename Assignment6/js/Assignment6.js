d145bb3e30d8807bac408d5aa69abef8a8db054e
cruggieri114 <32877728+cruggieri114@users.noreply.github.com>
2017-12-08 23:43:51 -0600

    added name for iOS

--- Assignment6/js/Assignment6.js
+++ Assignment6/js/Assignment6.js
@@ -660,9 +660,9 @@ function SearchContact(){
     var options      = new ContactFindOptions();
     options.filter   = lastname;
     options.multiple = true;
-    options.desiredFields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers];
+    options.desiredFields = [navigator.contacts.fieldType.id, navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers];
     options.hasPhoneNumber = true;
-    var fields = [navigator.contacts.fieldType.displayName];
+    var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
     navigator.contacts.find(fields, onSuccess, onError, options);
     
 function onSuccess(contacts) {
